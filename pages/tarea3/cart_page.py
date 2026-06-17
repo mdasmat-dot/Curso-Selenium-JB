e31988b5143f83eb438cad44653813b36a2bd03f
Mario Davila Asmat <mdasmat@gmail.com>
2026-06-16 21:51:07 -0500

    Proyecto Corregido

--- pages/tarea3/cart_page.py
+++ pages/tarea3/cart_page.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 class CartPage:
@@ -8,11 +10,18 @@ class CartPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def obtener_producto(self):
 
-        return self.driver.find_element(*self.ITEM_NAME).text
+        return self.driver.find_elements(*self.ITEM_NAME).text
+    
+        logger.info(f"Cantidad productos encontrados: {len(productos)}")
+
+        for i, p in enumerate(productos):
+            logger.info(f"Producto {i}: {p.text}")
+            return productos[0].text
 
     def checkout(self):
 
-        self.driver.find_element(*self.CHECKOUT_BUTTON).click()
+        self.wait.until(EC.element_to_be_clickable(self.CHECKOUT_BUTTON)).click()
